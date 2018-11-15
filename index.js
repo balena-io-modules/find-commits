e5ba2a0ef0bc8d10aa0d67a2ad2232f9f97167fe
Giovanni Garufi <nazrhom@gmail.com>
2018-11-15 10:51:43 +0100

    Initial
    Signed-off-by: Giovanni Garufi <giovanni@balena.io>

---
+++ index.js
@@ -0,0 +1,54 @@
+#!/usr/bin/env node
+
+'use strict'
+
+const capitano = require('capitano')
+const octokit = require('@octokit/rest')({
+  debug: !!process.env.DEBUG
+})
+
+octokit.authenticate({ type: 'token', token: process.env.GITHUB_TOKEN })
+
+capitano.command({
+  signature: 'pr',
+  options: [{
+    signature: 'repo',
+    parameter: 'repo',
+    required: true,
+    description: 'Head repository name',
+  }, {
+    signature: 'owner',
+    parameter: 'owner',
+    required: true,
+    description: 'Head owner name',
+  }, {
+    signature: 'number',
+    parameter: 'number',
+    required: true,
+    description: 'PR number',
+  }],
+  action: async (params, options) => {
+    const owner = options['owner']
+    const repo = options['repo']
+    const number = options['number']
+
+    const commits = await octokit.pullRequests.getCommits({
+      owner,
+      repo,
+      number
+    })
+    const messages = commits.data.map((commit) => {
+      return commit.commit.message
+    })
+    for (const msg of messages) {
+      console.log(msg)
+    }
+  }
+})
+
+
+capitano.run(process.argv, err => {
+  if (err != null) {
+    throw err
+  }
+})
